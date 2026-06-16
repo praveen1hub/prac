b07ad0801c2a848b0907dfedea8ff1eb10054b2b
praveenlattala <146346859+praveenlattala@users.noreply.github.com>
2026-06-16 12:45:32 +0530

    added all project files

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,5 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// https://vitejs.dev/config/
 export default defineConfig({
+  base: '/prac/',   // repo name
   plugins: [react()],
   css: {
     postcss: './postcss.config.js',

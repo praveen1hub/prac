2fa632daf8db30d4ade6abc43fffcf81db69ca93
praveenlattala <146346859+praveenlattala@users.noreply.github.com>
2026-06-18 09:29:59 +0530

    fix: quote repoName in vite config

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,15 @@
-const repoName = process.env.VITE_REPO_NAME || 'prac'
-const env = process.env.VITE_DEPLOY_ENV || 'prod'
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-base:
-  env === 'dev' ? `/${repoName}/dev/` :
-  env === 'uat' ? `/${repoName}/uat/` :
-  `/${prac}/`
+const repoName = process.env.VITE_REPO_NAME || 'prac'
+const env = process.env.VITE_DEPLOY_ENV || process.env.VITE_ENV || 'prod'
+
+export default defineConfig({
+  base:
+    env === 'dev'
+      ? `/${repoName}/dev/`
+      : env === 'uat'
+      ? `/${repoName}/uat/`
+      : `/${repoName}/`,
+  plugins: [react()],
+})

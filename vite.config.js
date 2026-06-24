5858bc4a8cfe3b93b2414328f59544ab972b800e
praveenlattala <146346859+praveenlattala@users.noreply.github.com>
2026-06-24 14:08:12 +0530

    deploy

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-const repoName = process.env.VITE_REPO_NAME || 'prac'
-const env = process.env.VITE_DEPLOY_ENV || process.env.VITE_ENV || 'prod'
-
 export default defineConfig({
-  base:
-    env === 'dev'
-      ? `/${repoName}/dev/`
-      : env === 'uat'
-      ? `/${repoName}/uat/`
-      : `/${repoName}/prod/`,
   plugins: [react()],
+  base: '/',
 })
